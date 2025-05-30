40b232c23e9776d61eab29c07fce47b95c09e756
vwkd <33468089+vwkd@users.noreply.github.com>
2025-05-30 17:03:27 +0200

    src: persist in Deno KV
    - since Deno Deploy may recreate worker which resets global variable and resends notification

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { getPage } from "./get.ts";
 import { sendNotification } from "./notify.ts";
 import { parsePage } from "./parse.ts";
 
+const KV_PREFIX = ["currentNum"];
+
 const CRON_SCHEDULE = Deno.env.get("CRON_SCHEDULE");
 
 if (!CRON_SCHEDULE) {
@@ -11,7 +13,7 @@ if (!CRON_SCHEDULE) {
 /**
  * Current latest number
  */
-let currentNum = "";
+let currentNum: string | null = null;
 
 /**
  * Check AnonymSMS page for latest number
@@ -19,7 +21,17 @@ let currentNum = "";
  * - sends a notification if it has changed.
  */
 async function checkNumber(): Promise<void> {
-  console.debug(`Checking current number '${currentNum}'`);
+  console.debug(`Checking current number`);
+
+  if (!currentNum) {
+    currentNum = (await kv.get<string>(KV_PREFIX)).value;
+  }
+
+  console.debug(
+    currentNum
+      ? `Got current number: '${currentNum}'`
+      : "Got no current number",
+  );
 
   const html = await getPage();
 
@@ -31,13 +43,16 @@ async function checkNumber(): Promise<void> {
 
   console.debug(`Got new number: '${num}'`);
 
-  if (num !== currentNum) {
+  if (!currentNum || num !== currentNum) {
     currentNum = num;
+    await kv.set(KV_PREFIX, num);
 
     await sendNotification(num, url);
   }
 }
 
 console.info(`Starting AnonymSMS notifier on schedule '${CRON_SCHEDULE}'...`);
+
+const kv = await Deno.openKv();
 
 Deno.cron("check latest number", CRON_SCHEDULE, checkNumber);
